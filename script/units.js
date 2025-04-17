7201621f31a48e730957f1b651f1e73b0233b4eb
magdur <magnus.duerr@assaabloy.com>
2025-04-17 14:55:24 +0200

    Support for unit variants: filtering of variants

--- script/units.js
+++ script/units.js
@@ -84,59 +84,82 @@ var units = {
         }
 
         data.units.forEach(function (unitSection) {
+            const parsedUnitsMap = unitSection.units.reduce((map, unit) => {
+                const split = unit.split('|');
+                map.set(split[0], split.slice(1)); // Use the name as the key and variants as the value
+                return map;
+            }, new Map());
+
             $.ajax("lists/" + unitSection.from, {
                 async: false,
                 success: function (units) {
                     units.forEach(function (section) {
-                        section.unit = section.unit.filter(function (unit) {
-                            return unitSection.units.includes(unit.name);
-                        });
+                        section.unit = section.unit
+                            .filter(function (unit) {
+                                return parsedUnitsMap.keys().toArray().includes(unit.name)
+                            })
+                            .map(function (unit) {
+                                if (unit.variants !== undefined &&
+                                    parsedUnitsMap.get(unit.name).length > 0 &&
+                                    parsedUnitsMap.get(unit.name).length !== unit.variants.length) {
 
-                        section.unit.sort(function (a, b) {
-                            typeSortNumber = typeSort(a.type) - typeSort(b.type)
-
-                            if (typeSortNumber !== 0) {
-                                return typeSortNumber
-                            } else {
-                                return a.name.localeCompare(b.name)
-                            }
-                        })
-
-                        section.unit.forEach(function (unit) {
-                            if (unit.weapons !== undefined) {
-                                unit.weapons = unit.weapons.map(weapon => replaceWeapon(weapon, weapons, unit));
-                            }
-                            else if (unit.variants !== undefined) {
-                                unit.variants.forEach(function (variant) {
-                                    variant.weapons = variant.weapons.map(weapon => replaceWeapon(weapon, weapons, unit));
-                                })
-                            }
-                        });
-
-                        section.specialRules.forEach(function (rule) {
-                            specialRules[rule.title] = rule
-                        })
-
-                        section.specialRules = unitSection.unitRules.map(function (rule) {
-                            if (specialRules[rule] !== undefined) {
-                                return specialRules[rule]
-                            } else {
-                                return {
-                                    "name": "ERROR",
-                                    "description": "Unknown special rule: " + rule
+                                    unit.variants = unit.variants.filter(function (variant) {
+                                        return parsedUnitsMap.get(unit.name).includes(variant.name)
+                                    });
                                 }
-                            }
-                        })
+
+                                return unit
+                            });
+
+
+
+                    section.unit.sort(function (a, b) {
+                        typeSortNumber = typeSort(a.type) - typeSort(b.type)
+
+                        if (typeSortNumber !== 0) {
+                            return typeSortNumber
+                        } else {
+                            return a.name.localeCompare(b.name)
+                        }
+                    })
+
+                    section.unit.forEach(function (unit) {
+                        if (unit.weapons !== undefined) {
+                            unit.weapons = unit.weapons.map(weapon => replaceWeapon(weapon, weapons, unit));
+                        } else if (unit.variants !== undefined) {
+                            unit.variants.forEach(function (variant) {
+                                variant.weapons = variant.weapons.map(weapon => replaceWeapon(weapon, weapons, unit));
+                            })
+                        }
                     });
 
-                    unitSections.push(units);
-                }
-            });
-        });
+                    section.specialRules.forEach(function (rule) {
+                        specialRules[rule.title] = rule
+                    })
 
-        return unitSections;
-    },
-};
+                    section.specialRules = unitSection.unitRules.map(function (rule) {
+                        if (specialRules[rule] !== undefined) {
+                            return specialRules[rule]
+                        } else {
+                            return {
+                                "name": "ERROR",
+                                "description": "Unknown special rule: " + rule
+                            }
+                        }
+                    })
+                });
+
+            unitSections.push(units);
+        }
+        });
+    }
+)
+;
+
+return unitSections;
+},
+}
+;
 
 function error(message) {
     units.errors.push(message);
