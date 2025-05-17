5f7ff7290848679808ba173978c1f38593f31c11
magdur <magnus.duerr@assaabloy.com>
2025-05-17 20:59:26 +0200

    Improving UI and migrating datamodel towards something usable by an army builder...

--- script/templating.js
+++ script/templating.js
@@ -28,6 +28,39 @@ var eaTemplating = {
             eaTemplating.templates['list'] = Handlebars.compile(template);
         });
 
+        Handlebars.registerHelper('appendPlural', function (number) {
+            return number > 1 ? "s" : "";
+        });
+
+        Handlebars.registerHelper('numberAsText', function (number, capitalize) {
+            function toText(number) {
+                if (number === 1) {
+                    return "one";
+                } else if (number === 2) {
+                    return "two";
+                } else if (number === 3) {
+                    return "three";
+                } else if (number === 4) {
+                    return "four";
+                } else if (number === 5) {
+                    return "five";
+                } else if (number === 6) {
+                    return "six";
+                } else if (number === 7) {
+                    return "seven";
+                } else if (number === 8) {
+                    return "eight";
+                } else if (number === 9) {
+                    return "nine";
+                } else if (number === 10) {
+                    return "ten";
+                }
+            }
+
+            const word = toText(number)
+            return capitalize ? word.charAt(0).toUpperCase() + word.slice(1) : word;
+        });
+
         Handlebars.registerHelper('oddEven', function (index) {
             return index % 2 === 0 ? 'even' : 'odd';
         });
